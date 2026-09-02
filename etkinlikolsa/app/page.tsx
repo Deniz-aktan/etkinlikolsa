2950d856e4f4b4a358acfaa9093d6600c645e370
Deniz-aktan <88589713+Deniz-aktan@users.noreply.github.com>
2026-09-02 18:11:28 +0300

    Update page.tsx

--- etkinlikolsa/app/page.tsx
+++ etkinlikolsa/app/page.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/Header";
+<Header />
 import Hero from "@/components/Hero";
 import Categories from "@/components/Categories";
 import FeaturedEvents from "@/components/FeaturedEvents";
